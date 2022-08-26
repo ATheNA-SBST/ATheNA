66ba794d5a5ca16e906ab64e03380bccf9f5f4d5
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-08-26 11:52:14 -0400

    Removing UpdateStaliro from path when configuring

--- src/configureAthena.m
+++ src/configureAthena.m
@@ -37,8 +37,10 @@ if nargin == 0
     [staliro_file_loc,~] = fileparts(staliro_file_loc);
 end
 cur_dir = pwd;
-addpath(genpath('UpdateStaliro'))
+folder_loc = which('configureAthena.m');
+[folder_loc,~] = fileparts(folder_loc);
+addpath(genpath(strcat(folder_loc,filesep,'UpdateStaliro')));
 updateStaliroCode(staliro_file_loc);
 cd(cur_dir);
-rmpath(genpath('UpdateStaliro'))
+rmpath(genpath(strcat(folder_loc,filesep,'UpdateStaliro')));
 end
